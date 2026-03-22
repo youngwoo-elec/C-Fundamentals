b7dfea0d8d019febb32e795afed41f5662272522
youngwoo-elec <youngwoo.elec@gmail.com>
2026-03-22 03:06:17 +0000

    Feat: practice character I/O using getchar and putchar

---
+++ 03-Pointers/string_test.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h> //isalpha() → 주어진 문자가 알파벳인지를 검사
+
+#define MAXSTRING  100
+
+int main(){
+    char c, name[MAXSTRING];
+    int i, sum =0;
+
+    printf("\nHi!   What's your name?  ");
+    for(i=0;(c=getchar())!='\n';++i){  //getchar() = 키보드부터 문자 딱 하나를 가져온다.
+        name[i]=c;
+        if(isalpha(c))
+            sum += c;
+    }
+    name[i] = '\0';
+    printf("\n%s%s%s\n%s",
+        "Nice to meet you ",name,".",
+        "Your name spelled backwards is ");
+    for(--i;i>=0;--i){
+        putchar(name[i]); //putchar = 한 글자씩 화면에 띄운다
+    }
+    printf("\n%s%d%s\n%s\n",
+        "and the letters in your name sum to ",sum,".",
+        "Have a nice day");
+    
+    return 0;
+
+}   
