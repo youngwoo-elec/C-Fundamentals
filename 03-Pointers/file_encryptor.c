77bd987e43cb78472f3078b21d16e0d8b9c6bcde
youngwoo-elec <youngwoo.elec@gmail.com>
2026-03-29 08:06:04 +0000

    Feat: encrypt ifp/with add key on ASCII

---
+++ 03-Pointers/file_encryptor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define KEY 3
+
+int main(int argc, char *argv[]) {
+    FILE *ifp, *ofp;
+    int c;
+
+    if (argc != 3) {
+        printf("Usage: %s <src> <dest>\n", argv[0]);
+        return 1;
+    }
+
+    ifp = fopen(argv[1], "r");
+    ofp = fopen(argv[2], "w");
+    
+    if (ifp == NULL || ofp == NULL) {
+        printf("Error: Could not open files.\n");
+        return 1;
+    }
+
+    while ((c = getc(ifp)) != EOF) {
+        // ASCII 값을 키만큼 밀어서 저장
+        putc(c + KEY, ofp);
+    }
+
+    fclose(ifp);
+    fclose(ofp);
+
+    printf("Encryption Complete!\n");
+    return 0;
+}
