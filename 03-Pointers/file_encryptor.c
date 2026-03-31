6b2f87c183a01566d3783bce53cd11cb41118138
youngwoo-elec <youngwoo.elec@gmail.com>
2026-03-31 09:55:19 +0000

    Feat: integrate encryption and decryption into a single utility

--- 03-Pointers/file_encryptor.c
+++ 03-Pointers/file_encryptor.c
@@ -6,27 +6,46 @@ int main(int argc, char *argv[]) {
     FILE *ifp, *ofp;
     int c;
 
-    if (argc != 3) {
-        printf("Usage: %s <src> <dest>\n", argv[0]);
+    if (argc != 4) {
+        printf("Usage: %s <e/d> <src> <dest>\n", argv[0]);
+        printf("    e: Encryption\n");
+        printf("    d: Decryption\n");
         return 1;
     }
 
-    ifp = fopen(argv[1], "rb");
-    ofp = fopen(argv[2], "wb");
+    ifp = fopen(argv[2], "rb");
+    ofp = fopen(argv[3], "wb");
     
     if (ifp == NULL || ofp == NULL) {
         printf("Error: Could not open files.\n");
         return 1;
     }
 
+    char mode = argv[1][0];
+
     while ((c = getc(ifp)) != EOF) {
         // ASCII 값을 키만큼 밀어서 저장
-        putc(c - KEY, ofp);
+        if(mode=='e'){
+            putc(c + KEY, ofp);
+        }
+        else if(mode=='d'){
+            putc(c - KEY, ofp);
+        }
+        else {
+            printf("[Error] Invalid mode: '%c'. Use 'e' or 'd'.\n", mode);
+            fclose(ifp);
+            fclose(ofp);
+            return 1;
+        }
     }
 
     fclose(ifp);
     fclose(ofp);
+    if(mode=='e')
+        printf("Encryption Complete!\n");
+    else
+        printf("decryption Complete!\n");
 
-    printf("Encryption Complete!\n");
+
     return 0;
 }
