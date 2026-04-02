f94e6a7a79fcaad712ebb0fa7c3c5c4269719c59
youngwoo-elec <youngwoo.elec@gmail.com>
2026-04-02 09:46:10 +0000

    Feat: return scanf & printf for int-type

---
+++ 01-Basic-Syntax/return_printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int cnt_s, cnt_p;
+    double a, b, c;
+    printf("a b c:");
+    cnt_s = scanf("%lf %lf %lf", &a, &b, &c);
+    printf("%lf %lf %lf\n", a, b, c);
+    cnt_p = printf("%lf %lf %lf\n", a, b, c);
+    printf("cnt_s: %d\n", cnt_s);
+    printf("cnt_p: %d\n", cnt_p);
+}
