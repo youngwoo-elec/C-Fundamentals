fc530e9d56857651da361a3c8caf6093e559daf4
youngwoo-elec <youngwoo.elec@gmail.com>
2026-04-05 06:02:29 +0000

    Feat: creat header file of quick_sort.c

--- 03-Pointers/array_quick_sort.c
+++ 03-Pointers/array_quick_sort.c
@@ -1,3 +1,4 @@
+#include "quick_sort.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
---
+++ 03-Pointers/quick_sort.h
@@ -0,0 +1,9 @@
+#ifndef QUICK_SORT_H
+#define QUICK_SORT_H
+
+void swap(int *a, int *b);
+int partition(int arr[], int low, int high);
+void quicksort(int arr[], int low, int high);
+
+
+#endif
