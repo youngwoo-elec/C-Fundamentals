254982c517b77caa9d0f220e4f98724b8db44344
youngwoo-elec <youngwoo.elec@gmail.com>
2026-04-05 03:31:35 +0000

    Feat:implement random number generator with range control and formatting

--- 02-Functions/prn_rand.c
+++ 02-Functions/prn_rand.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-int rand(void);
+#include <stdlib.h>
+#include <time.h>
 
 int main(){
+    srand(time(NULL));
     int i, n;
     printf("\n%s\n%s",
         "Some randomly distuibuted integers will be printed.",
@@ -10,7 +12,7 @@ int main(){
     for(i=0;i<n;++i){
         if(i%8==0)
             putchar('\n');
-        printf("%7d ", rand());
+        printf("%7d ", rand()%900+100);
     }
     printf("\n");
     return 0;
