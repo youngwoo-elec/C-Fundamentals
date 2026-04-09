6c5133016930fdd956ca4fe41110358f20f0d02c
youngwoo-elec <youngwoo.elec@gmail.com>
2026-04-09 10:19:18 +0000

    Refactor: fix format character lu -> zu

--- 01-Basic-Syntax/integral_promotion.c
+++ 01-Basic-Syntax/integral_promotion.c
@@ -4,11 +4,11 @@ int main(){
     char c = 'A';
     short s1 = 5, s2 = 6;
     printf("%c\n",c);
-    printf("size of c: %lu\n", sizeof(c));
-    printf("size of c+c: %lu\n", sizeof(c+c));
-    printf("size of s1: %lu\n",sizeof(s1));
-    printf("size of s2: %lu\n", sizeof(s2));
-    printf("size of s1+s2: %lu\n", sizeof(s1+s2));
+    printf("size of c: %zu\n", sizeof(c));
+    printf("size of c+c: %zu\n", sizeof(c+c));
+    printf("size of s1: %zu\n",sizeof(s1));
+    printf("size of s2: %zu\n", sizeof(s2));
+    printf("size of s1+s2: %zu\n", sizeof(s1+s2));
 
     return 0;
 
