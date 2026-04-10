2eb8ef33fb7c2e5d5c28c5cd9fede77415d9f4f1
youngwoo-elec <youngwoo.elec@gmail.com>
2026-04-10 10:18:48 +0000

    Fix: suppress tangent value explosion at 90 degrees

--- 01-Basic-Syntax/math.c
+++ 01-Basic-Syntax/math.c
@@ -4,10 +4,13 @@
 
 int main(){
     double ang = (M_PI/20);
-    printf("%3s%10s%10s%10s%10s\n","i","x", "Sin x","cos x", "tan x");
+    printf("%3s %10s %10s %10s %10s\n","i","x", "Sin x","cos x", "tan x");
     for(int i=0;i<=20;i++){
         double ang_f = ang*i;
-        printf("%3d%10.4f%10.4f%10.4f%10.4f\n",i,ang_f, sin(ang_f), cos(ang_f),tan(ang_f));
+        if(fabs(cos(ang_f))<1e-10) //fabs (절댓값): 0과 얼마나 가까운 지 보기 위함  1e-10: 1.0^(-10): 대충 0 정도
+            printf("%3d %10.4f %10.4f %10.4f %10s\n",i,ang_f, sin(ang_f), cos(ang_f),"INF");
+        else
+            printf("%3d %10.4f %10.4f %10.4f %10.4f\n",i,ang_f, sin(ang_f), cos(ang_f),tan(ang_f));
         
     }
 
