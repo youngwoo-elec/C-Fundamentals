c4e040603a2353be656df350bab9f898008623ca
youngwoo-elec <youngwoo.elec@gmail.com>
2026-04-18 08:15:55 +0000

    Feat: add assert practice and verify debug output

--- 02-Functions/functions.c
+++ 02-Functions/functions.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
+#include <assert.h>
+
+void t_putchar(void);
+void t_assert(void);
 
 
-void t_putchar(){
+int main(){
+    t_assert();
+
+    return 0;
+}
+
+void t_putchar(void){
     for(putchar('1');putchar('2');putchar('3')){
         putchar('4');
         continue;
         putchar('5');
     }
 }
-
-
-int main(){
-    t_putchar();
-
-    return 0;
+void t_assert(void){
+    int x = 5;
+    assert(x==5);
+    assert(x==10);
 }
