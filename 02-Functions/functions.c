976638cf71795f661460f4fe8af2828a801fd143
youngwoo-elec <youngwoo.elec@gmail.com>
2026-04-25 09:20:46 +0000

    Feat: implement reverse string output using recursion

--- 02-Functions/functions.c
+++ 02-Functions/functions.c
@@ -3,11 +3,13 @@
 
 void t_putchar(void);
 void t_assert(void);
+void wrt_it(void);
 
 
 int main(){
-    t_assert();
-
+    printf("Input a line: ");
+    wrt_it();
+    printf("\n");
     return 0;
 }
 
@@ -22,4 +24,12 @@ void t_assert(void){
     int x = 5;
     assert(x==5);
     assert(x==10); //an assertion
+}
+
+void wrt_it(void){
+    //재귀함수
+    int c; //EOF 검사 x → c를 char 형으로 선언해도 됨
+    if((c=getchar()) != '\n')
+        wrt_it();
+    putchar(c);
 }
